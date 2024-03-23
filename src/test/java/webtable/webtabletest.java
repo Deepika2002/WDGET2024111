53f245abc9d7873c6c13c7c0e66d6c345938ad46
Deepika2002 <asha03deepu@gmail.com>
2024-03-23 20:19:12 +0530

    second commit

--- src/test/java/webtable/webtabletest.java
+++ src/test/java/webtable/webtabletest.java
@@ -4,24 +4,24 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
 
 public class webtabletest {
-    public static void main(String[] args) {
-    	
+    private WebDriver driver;
+
+    @BeforeMethod
+    public void setUp() {
         System.setProperty("webdriver.chrome.driver", "C:/Users/deepika/Downloads/chromedriver-win64/chromedriver-win64/chromedriver.exe");
-
-       
-        WebDriver driver = new ChromeDriver();
-
-        
+        driver = new ChromeDriver();
         driver.get("https://www.w3schools.com/html/html_tables.asp");
+    }
 
-        
+    @Test(priority = 1)
+    public void printAllTableValues() {
         WebElement table = driver.findElement(By.xpath("//*[@id='customers']"));
-        
-        System.out.println("Web Table Traced");
 
-        // Retrieve Table Data
         // Iterate through each row and print cell values
         for (WebElement row : table.findElements(By.tagName("tr"))) {
             for (WebElement cell : row.findElements(By.tagName("td"))) {
@@ -30,7 +30,11 @@ public class webtabletest {
             }
             System.out.println(); // Move to the next row
         }
+    }
 
+
+    @AfterMethod
+    public void tearDown() {
         driver.quit();
     }
 }
